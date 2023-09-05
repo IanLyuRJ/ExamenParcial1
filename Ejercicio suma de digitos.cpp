f3d56562982979087a8227f8f775ee656030d538
Ian Rodriguez <126042348+IanLyuRJ@users.noreply.github.com>
2023-09-05 12:18:58 -0600

    Update Ejercicio suma de digitos.cpp

--- Ejercicio suma de digitos.cpp
+++ Ejercicio suma de digitos.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
-//Ejercicio "Programa que sima los digitos de un numero, esto hace que si pongo 123 = 6//
+//Ejercicio "Programa que suma los digitos de un numero, esto hace que si pongo 123 = 6//
 int main() {
     int numero = 0;
 	int suma = 0;
 	int digito = 0;
 	
-    printf("Escribe qué numero entero positivo de 2 DIGITOS quieres sumar (0 para salir): ");
+    printf("Escribe quÃ© numero entero positivo de 2 DIGITOS quieres sumar (0 para salir): ");
 	scanf("%d",&numero);
 	
 	while (numero >0) {
